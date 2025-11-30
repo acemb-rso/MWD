91ea664378f19c76fb96f32e0c826ccf270ec63b
acemb-rso <acember@iu.edu>
2025-11-30 16:35:27 -0500

    Fix battlemech quick actions

--- src/modules/actor/battlemech-sheet.js
+++ src/modules/actor/battlemech-sheet.js
@@ -13,6 +13,33 @@ export class BattlemechSheet extends VehicleSheet {
   activateListeners(html) {
     super.activateListeners(html);
 
+    html.find('[data-quick-actions] .quick-action-button').click(async event => {
+      event.stopPropagation();
+      event.preventDefault();
+      if (event.currentTarget.dataset.disabled === "true") return;
+      const action = event.currentTarget.dataset.action;
+      switch (action) {
+        case 'ranged':
+          await this.actor.rollRangedAttack();
+          break;
+        case 'melee':
+          await this.actor.rollMeleeAttack();
+          break;
+        case 'dodge':
+          await this.actor.rollDodge();
+          break;
+        case 'piloting':
+          await this.actor.rollPilotingCheck();
+          break;
+        case 'sensor':
+          await this.actor.rollSensorSweep();
+          break;
+        case 'repair':
+          await this.actor.rollEmergencyRepair();
+          break;
+      }
+    });
+
     html.find('.click-add-hardpoint').click(async event => {
       event.stopPropagation();
       const hardpoints = foundry.utils.deepClone(this.actor.system.mwd.hardpoints ?? []);
@@ -58,35 +85,6 @@ export class BattlemechSheet extends VehicleSheet {
       const weaponGroups = foundry.utils.deepClone(this.actor.system.mwd.weaponGroups ?? []);
       weaponGroups.forEach((group, idx) => group.isPrimary = idx === index ? event.currentTarget.checked : false);
       await this.actor.update({ 'system.mwd.weaponGroups': weaponGroups });
-    });
-  }
-
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    html.find('[data-quick-actions] .quick-action-button').click(async event => {
-      event.stopPropagation();
-      const action = event.currentTarget.dataset.action;
-      switch (action) {
-        case 'ranged':
-          await this.actor.rollRangedAttack();
-          break;
-        case 'melee':
-          await this.actor.rollMeleeAttack();
-          break;
-        case 'dodge':
-          await this.actor.rollDodge();
-          break;
-        case 'piloting':
-          await this.actor.rollPilotingCheck();
-          break;
-        case 'sensor':
-          await this.actor.rollSensorSweep();
-          break;
-        case 'repair':
-          await this.actor.rollEmergencyRepair();
-          break;
-      }
     });
   }
 }
