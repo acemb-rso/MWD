fa30e553fe7cb7c2506a15f52d7454094aa01e48
acemb-rso <acember@iu.edu>
2025-12-04 20:42:01 -0500

    Rename character actor label

--- src/modules/actor/character-npc-sheet.js
+++ src/modules/actor/character-npc-sheet.js
@@ -5,13 +5,26 @@ export class CharacterNPCSheet extends AnarchyActorSheet {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       width: 760,
-      height: 650
+      height: 650,
+      tabs: [
+        {
+          navSelector: '.npc-tabs',
+          contentSelector: '.npc-body',
+          initial: 'npc'
+        }
+      ]
     });
   }
 
   async _prepareContext(options) {
     const context = await super._prepareContext(options);
     context.options.classes = Array.from(new Set([...(context.options.classes ?? []), 'npc-sheet']));
+    context.npcItems = {
+      traits: context.items?.quality ?? [],
+      weapons: context.items?.weapon ?? [],
+      assetModules: context.items?.assetModule ?? [],
+      inventory: context.items?.gear ?? [],
+    };
     return context;
   }
 }
