a806fb24b93f1bc4bf0a8014d9697a112b20658e
acemb-rso <acember@iu.edu>
2025-12-07 18:11:46 -0500

    Pushing appv2 quick wins

--- src/modules/item/lifemodule-item-sheet.js
+++ src/modules/item/lifemodule-item-sheet.js
@@ -1,13 +1,5 @@
 import { BaseItemSheet } from "./base-item-sheet.js";
 
 export class LifeModuleItemSheet extends BaseItemSheet {
-
-  getData(options) {
-    const hbsData = super.getData(options);
-    return hbsData;
-  }
-
-  activateListeners(html) {
-    super.activateListeners(html);
-  }
+  // No overrides needed - BaseItemSheet handles everything
 }
--- src/modules/item/quality-item-sheet.js
+++ src/modules/item/quality-item-sheet.js
@@ -1,13 +1,5 @@
 import { BaseItemSheet } from "./base-item-sheet.js";
 
 export class QualityItemSheet extends BaseItemSheet {
-
-  getData(options) {
-    let hbsData = super.getData(options);
-    return hbsData;
-  }
-
-  activateListeners(html) {
-    super.activateListeners(html);
-  }
+  // No overrides needed - BaseItemSheet handles everything
 }
