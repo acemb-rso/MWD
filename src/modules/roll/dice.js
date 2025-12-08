fff1966772cac83d582af90d71a5964848f7149d
acemb-rso <acember@iu.edu>
2025-12-08 01:50:21 -0500

    Remove duplicate ANARCHY import

--- src/modules/roll/dice.js
+++ src/modules/roll/dice.js
@@ -1,6 +1,5 @@
 import { ANARCHY } from "../config.js";
 import { SYSTEM_DESCRIPTION, SYSTEM_NAME, THIRD_PARTY_STYLE_PATH } from "../constants.js";
-import { ANARCHY } from "../config.js";
 
 export const GLITCH_COLORSET = 'glitch';
 export const RISK_COLORSET = 'risk';
